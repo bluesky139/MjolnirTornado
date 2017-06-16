24cde4c97c50a8f0543787bb05758f98ce213ac2
bluesky139 <bluesky139@gmail.com>
2017-06-16 15:29:22 +0800

    Enable log.

--- main/main.py
+++ main/main.py
@@ -5,15 +5,29 @@ sys.path.insert(0, root_dir)
 
 import logging
 import time
-import redis
 import signal
 import json
 import tornado.web
 import tornado.escape
 from base import *
 from tornado.options import options
+
 options.parse_command_line(None, False)
+options.log_rotate_mode = 'time'
+options.log_rotate_when = 'midnight'
+options.log_rotate_interval = 1
+options.log_file_num_backups = 30
+if options.local_debug:
+	logging.getLogger().addHandler(logging.StreamHandler())
+	log_dir = root_dir + '/_log'
+	if not os.path.exists(log_dir):
+		os.mkdir(log_dir)
+	options.log_file_prefix = log_dir + '/' + str(options.port) + '.log'
+	options.working_dir = root_dir
+Assert(options.log_file_prefix, 'Please specify log_file_prefix.')
+Assert(options.working_dir, 'Please specify working_dir.')
 options.run_parse_callbacks()
+os.chdir(options.working_dir)
 
 from base.base_application import run
 apps = []
